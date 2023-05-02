180480699b1bd6513ae37653af9af7175769a809
Abdurasulov Aziz <azizabdurasulov2002@gmail.com>
2023-05-02 12:47:01 +0300

    postgres added

--- WISDjango/settings.py
+++ WISDjango/settings.py
@@ -78,13 +78,13 @@ WSGI_APPLICATION = 'WISDjango.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {'default': {
-    'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': BASE_DIR / 'db.sqlite3'},
-
-    'db2': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db_db2.sqlite3',
-    }}
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'postgres',
+    'USER': 'postgres',
+    'PASSWORD': 'admin',
+    'HOST': 'localhost',
+    'PORT': 5432}
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
