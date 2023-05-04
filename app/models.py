2a5f4bfacc842ff9b4e41747b2e298ecfc2d86e3
Abdurasulov Aziz <azizabdurasulov2002@gmail.com>
2023-05-04 13:08:50 +0300

    Profile model added

--- app/models.py
+++ app/models.py
@@ -9,13 +9,11 @@ from django.db import models
 
 from .manager import CustomUserManager
 
+
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField('email address', unique=True)
     first_name = models.CharField('first name', blank=True)
     last_name = models.CharField('last_name', blank=True)
-    phone = models.CharField('Phone', validators=[RegexValidator('[+]\d+$')], blank=True)
-    birth_day = models.DateField('Birth day', blank=True, null=True)
-    region = models.CharField('Region', blank=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(default=timezone.now)
@@ -25,10 +23,20 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     objects = CustomUserManager()
 
+    def __str__(self):
+        return self.email
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    phone = models.CharField('Phone', validators=[RegexValidator('[+]\d+$')], blank=True)
+    birth_day = models.DateField('Birth day', blank=True, null=True)
+    region = models.CharField('Region', blank=True)
+
     @property
     def age(self):
-        result = datetime.date.today() - self.birth_day #TODO
+        result = datetime.date.today() - self.birth_day
         return result.days // 365
 
     def __str__(self):
-        return self.email
+        return self.user.email
