7d7e0a9f941125c3ed54df469a84891f246bf21c
Abdurasulov Aziz <azizabdurasulov2002@gmail.com>
2023-05-23 13:21:27 +0300

    models structer changed | model's admin options added

--- app/admin.py
+++ app/admin.py
@@ -5,23 +5,62 @@ from app import models
 
 @admin.register(models.Food)
 class FoodAdmin(admin.ModelAdmin):
-    list_display = ('name', '')
+    list_display = ('name', 'price', 'category', 'spicy', 'on_stop', )
+    search_fields = ('name',)
+    list_filter = ('category', 'spicy', 'on_stop',)
+
+
+class DrinkAdminFilter(admin.SimpleListFilter):
+    title = 'Filters'
+    parameter_name = 'filter'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('Hot', 'Hot Drinks'),
+            ('Cold', 'Cold Drinks'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'Hot':
+            return queryset.filter(
+                category='Hot'
+            )
+        if self.value() == 'Cold':
+            return queryset.filter(
+                category='Cold'
+            )
 
 
 @admin.register(models.Drink)
 class DrinkAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'price', 'liter', 'category', 'on_stop',)
+    search_fields = ('name',)
+    list_filter = (DrinkAdminFilter,)
 
 
 @admin.register(models.Sauce)
 class SauceAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'price', 'on_stop',)
+    search_fields = ('name',)
+
 
 @admin.register(models.Dessert)
 class DessertAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'price', 'on_stop',)
+    search_fields = ('name',)
 
 
-@admin.register(models.IceCream)
-class IceCreamAdmin(admin.ModelAdmin):
-    pass
+@admin.register(models.BoxMix)
+class BoxMixAdmin(admin.ModelAdmin):
+    list_display = ('name', 'food', 'drink', 'sauce', 'dessert', 'on_stop', '_count_price')
+
+    readonly_fields = ('_count_price',)
+
+    def _count_price(self, obj: models.BoxMix):
+        return sum([
+            obj.food.price,
+            obj.drink.price,
+            obj.sauce.price,
+            obj.dessert.price
+        ]) / 100 * 85
+
--- app/models.py
+++ app/models.py
@@ -2,31 +2,42 @@ from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
 
+from app.managers import UnstopListDishManager
 
-class DishAbstractModel(models.Model):
-    name = models.CharField()
-    price = models.IntegerField()
 
-    def __str__(self):
-        return self.name
+class PriceMixinFieldModel(models.Model):
+    price = models.IntegerField(default=0)
 
     class Meta:
         abstract = True
 
 
-class Sauce(DishAbstractModel):
+class NameMixinFieldModel(models.Model):
+    name = models.CharField()
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class OnStopMixinFieldModel(models.Model):
+    on_stop = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+
+class Sauce(NameMixinFieldModel, PriceMixinFieldModel, OnStopMixinFieldModel):
     pass
 
 
-class Dessert(DishAbstractModel):
+class Dessert(NameMixinFieldModel, PriceMixinFieldModel, OnStopMixinFieldModel):
     pass
 
 
-class IceCream(DishAbstractModel):
-    pass
-
-
-class Food(DishAbstractModel):
+class Food(NameMixinFieldModel, PriceMixinFieldModel, OnStopMixinFieldModel):
     TYPE_DISH = (
         ('Burger', 'Burger'),
         ('Twister', 'Twister'),
@@ -36,7 +47,7 @@ class Food(DishAbstractModel):
     spicy = models.BooleanField(default=False)
 
 
-class Drink(DishAbstractModel):
+class Drink(NameMixinFieldModel, PriceMixinFieldModel, OnStopMixinFieldModel):
     TYPE_DRINK = (
         ('Cold', 'Cold Drinks'),
         ('Hot', 'Hot Drinks'),
@@ -46,4 +57,12 @@ class Drink(DishAbstractModel):
     liter = models.FloatField()
     category = models.CharField(choices=TYPE_DRINK)
 
+    def __str__(self):
+        return f'{self.name} {self.liter}'
 
+
+class BoxMix(OnStopMixinFieldModel, NameMixinFieldModel):
+    food = models.ForeignKey(Food, on_delete=models.DO_NOTHING)
+    drink = models.ForeignKey(Drink, on_delete=models.DO_NOTHING, blank=True)
+    sauce = models.ForeignKey(Sauce, on_delete=models.DO_NOTHING, blank=True)
+    dessert = models.ForeignKey(Dessert, on_delete=models.DO_NOTHING, blank=True)
